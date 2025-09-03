451fd803b839f6731f858d1218f6b26125a4e133
Oliver Saravia Gerson Adrian <gersoneitor67n@gmail.com>
2025-09-03 11:19:48 -0400

    saludo.java

---
+++ saludo.java
@@ -0,0 +1,12 @@
+package Saludo;
+
+public class saludo {
+
+	public static void main(String[] args) {
+		 System.out.println("Oliver Saravia Gerson Adrian C.I.:7362463");
+		 System.out.println("Mamani Calle Alex C.I.: 13280115");
+		 System.out.println("Paredes Quino Noe Roger C.I.:10076714");
+		 System.out.println("Quisbert Manrriquez Miguel Vicente C.I.: 10075850");
+	}
+
+}
