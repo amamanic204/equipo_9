ea811b11c5e63112dc14a49becd6f9eeea4b4dd3
QuisbertManrriquezMiguelVicente <mikiviquisbert@gmail.com>
2025-09-03 14:21:43 -0400

    archivo py

---
+++ operacion.py
@@ -0,0 +1,9 @@
+# Operació simple
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+print("La suma de",numero1,"y",numero2,"es",suma)
+print("1 Quisbert Manrriquez Miguel Vicente")
+print("2 Mamani Calle Alex")
+print("3 Oliver Saravia Gerson Adrian")
+print("4 Paredes Quino Noe Roger")
